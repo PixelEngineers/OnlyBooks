ad0d689fe115437aae68820715fbd6c03535715d
FloatingComet62 <shubh2007rai@gmail.com>
2024-10-06 14:22:21 +0530

    Id linking and stuff

--- OnlyBooks/app/views.py
+++ OnlyBooks/app/views.py
@@ -42,32 +42,56 @@ def books(request):
         "books": books_data
     })
 
+def map_review(review):
+    review["rating"] = '⭐' * review["rating"]
+    user_id = review.get("user_id")
+    if user_id is not None:
+        review["user_link"] = f'/profile/{user_id}'
+    book_id = review.get("book_id")
+    if book_id is not None:
+        review["book_link"] = f'/book/{book_id}'
+    return review
+
 def book(request, book_id):
     page = 'book'
     reviews = [{
         "message": "love this author's work",
         "rating": 5,
+        "username": "Jerry Doe",
+        "user_id": 1,
     }, {
         "message": "everything was amazing, but chapter 7 could have been better",
         "rating": 4,
+        "username": "Jessi Doe",
+        "user_id": 2,
     }]
     return render(request, 'app/book.html', {
         "page": page,
-        "reviews": reviews
+        "reviews": list(map(
+            lambda review: map_review(review),
+            reviews
+        ))
     })
 
-def profile(request):
+def profile(request, user_id):
     page = 'profile'
     reviews = [{
         "message": "best shit ever",
-        "rating": 5
+        "rating": 5,
+        "book_name": "Baby Shark",
+        "book_id": 0
     }, {
         "message": "what the hell is this",
-        "rating": 1
+        "rating": 1,
+        "book_name": "Adult Shark",
+        "book_id": 1
     }]
     username = "John Doe"
     return render(request, 'app/profile.html', {
         "page": page,
         "username": username,
-        "reviews": reviews
+        "reviews": list(map(
+            lambda review: map_review(review),
+            reviews
+        ))
     })
